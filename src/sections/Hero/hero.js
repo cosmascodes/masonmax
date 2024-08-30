2ddba8b7c583cc3b7ac5e1f828ddd49d8fd6a85f
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2024-08-31 00:17:39 +0300

    Push

--- src/sections/Hero/hero.js
+++ src/sections/Hero/hero.js
@@ -18,7 +18,7 @@ const Hero = () => {
           </p>
           <a
             href="#"
-            class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            class="mb-6 md:mb-0 inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
           >
             Start Your Project
             <svg
